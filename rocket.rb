2ef233fe9574f6f88344572b9f91e064ec3acb18
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2023-06-06 16:23:04 -0400

    Add documentation for `Rocket`

--- rocket.rb
+++ rocket.rb
@@ -1,7 +1,47 @@
 # frozen_string_literal: true
+
 require_relative './launch_control'
 
-# TODO: add documentation for class
+# Rocket class represents a rocket used for space missions.
+#
+# Usage:
+#   rocket = Rocket.prepare_for_launch(
+#     distance: 160,
+#     burn_rate: 168_240,
+#     average_speed: 1500
+#   )
+#
+#   rocket.launch? && rocket.launch do |status|
+#     # Perform actions based on the mission status
+#     # For example, display the status to the console
+#     puts status
+#   end
+#
+#   rocket.summary # Retrieve the summary of the mission after completion
+#
+# Attributes:
+#   - distance: The total distance the rocket needs to travel.
+#   - burn_rate: The fuel burn rate of the rocket in liters per minute.
+#   - average_speed: The average speed of the rocket in kilometers per hour.
+#
+# Public Class Methods:
+#   - prepare_for_launch: Creates a new instance of the Rocket class and prepares it for launch.
+#
+# Public Instance Methods:
+#   - launch?: Checks if the rocket is ready for launch.
+#   - launch: Initiates the rocket launch and executes the given block of code for each iteration.
+#   - summary: Retrieves the summary of the mission after completion.
+#
+# Private Instance Methods:
+#   - explode_iteration: Calculates the iteration at which the rocket will explode, if applicable.
+#   - flight_time: Calculates the elapsed flight time of the rocket.
+#   - status: Retrieves the current status of the rocket, including elapsed time, distance traveled, etc.
+#   - reached_destination?: Checks if the rocket has reached its destination.
+#   - current_speed: Calculates the current speed of the rocket in kilometers per minute.
+#   - calculate_time_to_destination: Calculates the estimated time remaining to reach the destination.
+#   - calculate_distance_traveled: Calculates the distance traveled by the rocket based on the elapsed time.
+#   - calculate_total_fuel_burned: Calculates the total amount of fuel burned by the rocket.
+#
 class Rocket
   attr_reader :distance, :burn_rate,
               :average_speed, :distance_traveled, :elapsed_time
