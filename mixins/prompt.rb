4f15061f658383ad0cb7e5e4935f95a45766ecc2
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2023-06-06 16:53:34 -0400

    Add documentation string for `Prompt` module

--- mixins/prompt.rb
+++ mixins/prompt.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-# TODO: add documentation string
+# Prompt module provides a utility method for prompting user input with a yes/no question.
+#
+# Usage:
+#   include Prompt
+#
+#   if prompt('Continue?')
+#     # Perform actions if user chooses to continue
+#   else
+#     # Perform actions if user chooses not to continue
+#   end
+#
+# Public Instance Methods:
+#   - prompt: Prompts the user with a yes/no question and returns a boolean value indicating the choice.
+#
 module Prompt
   def prompt(message)
     print "#{message} (Y/n) "
