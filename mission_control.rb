2bfad06b0b39f01225fb469db55da3e79ff99753
Ghulam Murtaza <gmurtaza.va@gmail.com>
2023-06-06 14:23:51 -0400

    Add start logic to `MissionControl#start`

--- mission_control.rb
+++ mission_control.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative './mission'
+
 # TODO: add documentation for class
 class MissionControl
   def initialize
@@ -10,8 +12,27 @@ class MissionControl
     new.start
   end
 
-  # Entry point to mission control
   def start
-    # TODO: Entry point logic here
+    puts 'Welcome to Mission Control!'
+    loop do
+      break unless run_mission
+    end
+
+    # TODO: logic for displaying the summary
+    puts 'Exiting Mission Control. Goodbye!'
+  end
+
+  private
+
+  def run_mission
+    mission = Missiٍon.new
+
+    mission.print_plan
+    mission.fetch_mission_name
+
+    # Logic for running the mission here
+
+    @missions << mission
+    prompt('Would you like to run another mission?')
   end
 end
