647333244cfc4bd750a8507de6c4ed5c0411bd5d
MuhammadAmir5670 <muhammadamir5670@gmail.com>
2023-06-06 14:47:13 -0400

    Refactor interface for the `Rocket`

--- rocket.rb
+++ rocket.rb
@@ -2,10 +2,23 @@
 
 # TODO: add documentation for class
 class Rocket
-  def initialize(distance:, burn_rate:, average_speed:)
+  def initialize(distance, burn_rate, average_speed)
+    @launch_control = LaunchControl.new
     @distance = distance
     @burn_rate = burn_rate
     @average_speed = average_speed
     @distance_traveled = 0
+  end
+
+  def self.prepare_for_launch(distance:, burn_rate:, average_speed:)
+    rocket = new(distance, burn_rate, average_speed)
+
+    prepare_for_launch
+
+    rocket
+  end
+
+  def prepare_for_launch
+    @launch_control.prepare_for_launch
   end
 end
